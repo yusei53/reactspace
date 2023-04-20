b21b5002a42c8b627c9218b9bf8e616944f8bf13
今村悠聖 <yusei@imamurayuushounoMacBook-Air.local>
2023-04-20 17:47:03 +0900

    styled componentの記法

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import { InlineStyle } from './components/InlineStyle';
 import { Css } from './components/cssModules';
 import { StyledJsx } from './components/styledJsx';
+import { StyledComponent } from './components/StyledComponent';
 
 export function App() {
 
@@ -14,6 +15,7 @@ export function App() {
      <InlineStyle />
      <Css />
      <StyledJsx />
+     <StyledComponent />
     </div>
   );
 }
